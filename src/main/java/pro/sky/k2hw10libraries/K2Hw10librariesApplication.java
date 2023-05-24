85ddb21d044396305770d76eb823eed0042333d9
Prudnikova <dakoto4ka@yandex.ru>
2023-05-24 17:20:30 +0300

    первый коммит

---
+++ src/main/java/pro/sky/k2hw10libraries/K2Hw10librariesApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.k2hw10libraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K2Hw10librariesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K2Hw10librariesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/k2hw10libraries/K2Hw10librariesApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.k2hw10libraries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K2Hw10librariesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
