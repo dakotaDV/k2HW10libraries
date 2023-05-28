39dbd22fdd4bf24f75e10653f2fc7587b48b78e6
Prudnikova <dakoto4ka@yandex.ru>
2023-05-28 16:54:39 +0300

    добавила ValidatorService

--- src/main/java/pro/sky/k2hw10libraries/service/ValidatorService.java
+++ src/main/java/pro/sky/k2hw10libraries/service/ValidatorService.java
@@ -18,7 +18,8 @@ return StringUtils.capitalize(name.toLowerCase());
     public String validateSurname(String surname){
 String[]surnames = surname.split("-");
 for(int i = 0;i<surnames.length;i++){
-    if(!StringUtils.isAlpha(surnames[i])){
+   if(!StringUtils.isAlpha(surnames[i])){
+
         throw new IncorrectNameException(surname);
     }
     surnames[i] = StringUtils.capitalize(surnames[i].toLowerCase());
